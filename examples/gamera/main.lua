be4c762cf9e7c66d74ecad9fb3b71823a0c1c9c5
volkov <volkovissocool@gmail.com>
2023-11-19 17:51:17 +0200

    Initial hump camera example.

--- examples/gamera/main.lua
+++ examples/gamera/main.lua
@@ -1,5 +1,6 @@
 -- Gamera.lua - https://github.com/kikito/gamera, MIT License.
 
+-- Setup resolution solution.
 local rs = require("resolution_solution")
 rs.conf({game_width = 640, game_height = 480, scale_mode = 1})
-
+rs.setMode(rs.game_width, rs.game_height, {resizable = true})
---
+++ examples/hump_camera/main.lua
@@ -0,0 +1,3 @@
+-- camera.lua - https://github.com/vrld/hump/blob/master/camera.lua, by Matthias Richter
+
+
